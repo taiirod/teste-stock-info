d87fdae76d70fe57457b31b15dc28ce344cebacf
Tainan Rodrigues <tainansilvasci@gmail.com>
2019-08-07 23:52:40 -0300

    classe Conta criada

--- backend/src/main/java/com/teste/stockinfo/controller/ContaController.java
+++ backend/src/main/java/com/teste/stockinfo/controller/ContaController.java
@@ -6,6 +6,9 @@ import com.teste.stockinfo.model.Deposito;
 import com.teste.stockinfo.model.Saque;
 import com.teste.stockinfo.model.enums.TipoContaEnum;
 import com.teste.stockinfo.repository.ContaRepository;
+import com.teste.stockinfo.repository.DepositoRepository;
+import com.teste.stockinfo.repository.SaqueRepository;
+import com.teste.stockinfo.service.ContaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,9 @@ public class ContaController {
 
     @Autowired
     private ContaRepository contaRepository;
+
+    @Autowired
+    private ContaService contaService;
 
     @GetMapping
     public List<Conta> buscarTodos() {
@@ -36,56 +42,15 @@ public class ContaController {
         return ResponseEntity.ok(contaNova);
     }
 
-    @PutMapping("/sacar/{idConta}")
-    public ResponseEntity<Conta> sacar(@PathVariable Conta idConta, @RequestBody Saque saque) {
-        Conta c = contaRepository.getOne(idConta.getId());
-
-        if (saque.getTipoDeConta().equals(TipoContaEnum.Eventual)) {
-            if (saque.getValor() < c.getSaldoContaEventual()) {
-                Double saldoAtual = c.getSaldoContaEventual();
-                saldoAtual = saldoAtual - saque.getValor();
-
-                c.setSaldoContaEventual(saldoAtual);
-
-                contaRepository.save(c);
-            }
-        } else if (saque.getTipoDeConta().equals(TipoContaEnum.Normal)) {
-            if (saque.getValor() < c.getSaldoContaNormal()) {
-                Double saldoAtual = c.getSaldoContaNormal();
-                saldoAtual = saldoAtual - saque.getValor();
-
-                c.setSaldoContaNormal(saldoAtual);
-
-                contaRepository.save(c);
-            }
-        }
-
-
-        return ResponseEntity.ok(c);
-    }
-
     @PutMapping("/depositar/{idConta}")
     public ResponseEntity<Conta> depositar(@PathVariable Conta idConta, @RequestBody Deposito deposito) {
-        Conta c = contaRepository.getOne(idConta.getId());
-
-        if (deposito.getTipoDeConta().equals(TipoContaEnum.Eventual)) {
-            Double saldoAtual = c.getSaldoContaEventual();
-            saldoAtual = saldoAtual + deposito.getValor();
-
-            c.setSaldoContaEventual(saldoAtual);
-
-            contaRepository.save(c);
-        } else if (deposito.getTipoDeConta().equals(TipoContaEnum.Normal)) {
-            Double saldoAtual = c.getSaldoContaNormal();
-            saldoAtual = saldoAtual + deposito.getValor();
-
-            c.setSaldoContaNormal(saldoAtual);
-
-            contaRepository.save(c);
-        }
-
-
-        return ResponseEntity.ok(c);
+        return contaService.depositar(idConta, deposito);
     }
+
+    @PutMapping("/sacar/{idConta}")
+    public ResponseEntity<Conta> sacar(@PathVariable Conta idConta, @RequestBody Saque saque) {
+        return contaService.sacar(idConta, saque);
+    }
+
 
 }
---
+++ backend/src/main/java/com/teste/stockinfo/service/ContaService.java
@@ -0,0 +1,84 @@
+package com.teste.stockinfo.service;
+
+import com.teste.stockinfo.model.Conta;
+import com.teste.stockinfo.model.Deposito;
+import com.teste.stockinfo.model.Saque;
+import com.teste.stockinfo.model.enums.TipoContaEnum;
+import com.teste.stockinfo.repository.ContaRepository;
+import com.teste.stockinfo.repository.DepositoRepository;
+import com.teste.stockinfo.repository.SaqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class ContaService {
+
+
+    @Autowired
+    private ContaRepository contaRepository;
+
+    @Autowired
+    private DepositoRepository depositoRepository;
+
+    @Autowired
+    private SaqueRepository saqueRepository;
+
+    public ResponseEntity<Conta> sacar(@PathVariable Conta idConta, @RequestBody Saque saque) {
+        Conta c = contaRepository.getOne(idConta.getId());
+
+        saque.setConta(c);
+
+        if (saque.getTipoDeConta().equals(TipoContaEnum.Eventual)) {
+            if (saque.getValor() < c.getSaldoContaEventual()) {
+                Double saldoAtual = c.getSaldoContaEventual();
+                saldoAtual = saldoAtual - saque.getValor();
+
+                c.setSaldoContaEventual(saldoAtual);
+
+                contaRepository.save(c);
+            }
+        } else if (saque.getTipoDeConta().equals(TipoContaEnum.Normal)) {
+            if (saque.getValor() < c.getSaldoContaNormal()) {
+                Double saldoAtual = c.getSaldoContaNormal();
+                saldoAtual = saldoAtual - saque.getValor();
+
+                c.setSaldoContaNormal(saldoAtual);
+
+                contaRepository.save(c);
+            }
+        }
+
+        saqueRepository.save(saque);
+
+        return ResponseEntity.ok(c);
+    }
+
+    public ResponseEntity<Conta> depositar(@PathVariable Conta idConta, @RequestBody Deposito deposito) {
+        Conta c = contaRepository.getOne(idConta.getId());
+
+        deposito.setConta(c);
+
+        if (deposito.getTipoDeConta().equals(TipoContaEnum.Eventual)) {
+            Double saldoAtual = c.getSaldoContaEventual();
+            saldoAtual = saldoAtual + deposito.getValor();
+
+            c.setSaldoContaEventual(saldoAtual);
+
+            contaRepository.save(c);
+        } else if (deposito.getTipoDeConta().equals(TipoContaEnum.Normal)) {
+            Double saldoAtual = c.getSaldoContaNormal();
+            saldoAtual = saldoAtual + deposito.getValor();
+
+            c.setSaldoContaNormal(saldoAtual);
+
+            contaRepository.save(c);
+        }
+
+        depositoRepository.save(deposito);
+
+        return ResponseEntity.ok(c);
+    }
+}
